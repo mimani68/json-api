c07927b9c3e7afe38455f8f8df72db7590338818
mahdi <imani.mahdi@gmail.com>
2020-01-09 11:02:25 +0330

    feat: update seriallizer

--- seriallizer/use.seriallizer.js
+++ seriallizer/use.seriallizer.js
@@ -1,8 +1,8 @@
 
+var JSONAPISerializer = require('jsonapi-serializer').Serializer;
+
 var contact = require('../db/db').contact;
 contactAfterQuery = id => contact.filter( x => x.id === id )
-
-var JSONAPISerializer = require('jsonapi-serializer').Serializer;
 
 var contctSerializer = new JSONAPISerializer('contact', {
 	attributes: ['title', 'identity']
@@ -14,14 +14,15 @@ const userOptionsSeriallizer = {
 		source: 'contact'
 	}],
 }
-var userSerial = new JSONAPISerializer('users', userOptionsSeriallizer);
 
 module.exports.userSeriallizer = function (data) {
+	var userSerial = new JSONAPISerializer('users', userOptionsSeriallizer);
 	let temp = userSerial.serialize(data)
 	temp.includes = []
 	temp.includes.push(
 		contctSerializer.serialize( contactAfterQuery(data[0].contact) ).data
 	)
+	temp.links = {}
 	temp.data[0].relationships = []
 	temp.data[0].relationships.push({
 		[userOptionsSeriallizer.relationshipMeta[0].source]: {
